9eea164a003598e2874d3c2d6e556472cb3da153
Ruchir <@ruchirlives>
2026-04-30 16:47:29 +0100

    Implement file drag-and-drop functionality in MainComponent

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -10,7 +10,8 @@
 #include "AudioUdpStreamer.h"
 
 class MainComponent :   public juce::Component, 
-                        public juce::ComboBox::Listener
+                        public juce::ComboBox::Listener,
+                        public juce::FileDragAndDropTarget
 {
 public:
     struct RestoreModalController
@@ -30,6 +31,10 @@ public:
 
     void paint(juce::Graphics& g) override;
     void resized() override;
+	bool isInterestedInFileDrag(const juce::StringArray& files) override;
+	void fileDragEnter(const juce::StringArray& files, int x, int y) override;
+	void fileDragExit(const juce::StringArray& files) override;
+	void filesDropped(const juce::StringArray& files, int x, int y) override;
 
 	void updateOverdubUI();
 	void updateOpenPluginButtonState();
@@ -107,9 +112,11 @@ public:
 	void refreshOrchestraTableUI();
 	void replacePluginForRow(int row, juce::Component* anchor);
 	void applyPluginReplacement(int row, const juce::String& pluginName);
+	void addInstrumentFromNkiFile(const juce::File& nkiFile);
 
 private:
     juce::File pluginFolder; // Use a juce::File object instead of a pointer
+	bool isProjectFileDragOver = false;
 
     LayoutMetrics getLayoutMetrics() const;
 
